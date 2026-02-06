1ff898901f85351db5fe96d2382a2c7595d15bed
Ali Zareian <hqx131@gmail.com>
2026-02-06 05:42:54 -0800

    first commit

---
+++ TachraFac.Core/Services/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TachraFac.Core.DTOs;
+using TachraFac.Datalayer.Entities.User;
+
+namespace TachraFac.Core.Services.Interfaces
+{
+    public interface IUserService
+    {
+        bool IsUserNameExist(string userName);
+        bool IsEmailExist(string email);
+        int AddUser(User user);
+        User LoginUser(LoginViewModel login);
+        bool ActiceAccount(string activeCode);
+        User GetUserByUserName(string username);
+        User GetUserByActiveCode(string activeCode);
+        void UpdateUser(User user);
+    }
+}
---
+++ TachraFac.Core/Services/UserService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TachraFac.Core.DTOs;
+using TachraFac.Core.Genrator;
+using TachraFac.Core.Security;
+using TachraFac.Core.Services.Interfaces;
+using TachraFac.Datalayer.Context;
+using TachraFac.Datalayer.Entities.User;
+
+namespace TachraFac.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private TachraContext _context;
+        public UserService(TachraContext context)
+        {
+            _context = context;
+        }
+
+        public bool ActiceAccount(string activeCode)
+        {
+            var user = _context.tblUser.SingleOrDefault(u => u.ActiveCode == activeCode);
+            if (user == null || user.IsActive)
+            {
+                return false;
+            }
+            user.IsActive = true;
+            user.ActiveCode = NameGenerator.GenerateUnicCode();
+            _context.SaveChanges();
+            return true;
+        }
+
+        public int AddUser(User user)
+        {
+            _context.tblUser.Add(user);
+            _context.SaveChanges();
+            return user.UserId;
+        }
+
+        public User GetUserByActiveCode(string activeCode)
+        {
+            return _context.tblUser.SingleOrDefault(u => u.ActiveCode == activeCode);
+        }
+
+        public User GetUserByUserName(string username)
+        {
+            return _context.tblUser.SingleOrDefault(u => u.UserName == username);
+        }
+
+        public bool IsEmailExist(string email)
+        {
+            return _context.tblUser.Any(u => u.Email == email);
+        }
+
+        public bool IsUserNameExist(string userName)
+        {
+            return _context.tblUser.Any(u => u.UserName == userName);
+        }
+
+        public User LoginUser(LoginViewModel login)
+        {
+            string hashPassword = PasswordHelper.EncodePasswordMd5(login.Password);
+            string username = login.UserName;
+            return _context.tblUser.SingleOrDefault(u => u.UserName == username && u.Password == hashPassword);
+
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Update(user);
+            _context.SaveChanges();
+        }
+    }
+}
