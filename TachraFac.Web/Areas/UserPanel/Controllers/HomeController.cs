f629f2c5c185209daf2f104187dbe436796ab734
Ali Zareian <hqx131@gmail.com>
2026-02-07 14:51:08 -0800

    EditUserPanel

--- TachraFac.Web/Areas/UserPanel/Controllers/HomeController.cs
+++ TachraFac.Web/Areas/UserPanel/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TachraFac.Core.Services.Interfaces;
 
 namespace TachraFac.Web.Areas.UserPanel.Controllers
 {
     [Area("UserPanel")]
+    [Authorize]
     public class HomeController : Controller
     {
+        private IUserService _userService;
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_userService.GetUserByUserName(User.Identity.Name));
         }
     }
 }
